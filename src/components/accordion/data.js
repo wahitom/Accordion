c395ea85a5a013239f3d976d7501a89a1578e8df
Miriam Wahito Maina <miriammaina51@gmail.com>
2024-05-23 21:16:26 +0300

    Initial commit

---
+++ src/components/accordion/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+  },
+  {
+    id: 2,
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+  },
+  {
+    id: 3,
+    question: "Who wrote 'To Kill a Mockingbird'?",
+    answer: "Harper Lee",
+  },
+  {
+    id: 4,
+    question: "What is the chemical symbol for water?",
+    answer: "H2O",
+  },
+  {
+    id: 5,
+    question: "How many continents are there on Earth?",
+    answer: "Seven",
+  },
+];
+
+export default data;
---
+++ src/components/accordion/index.js
@@ -0,0 +1,9 @@
+//  thngs we are going to be able to do with our accordion
+// handle opeing a single selection
+// handle opening multiple selections
+
+export default function Accordion() {
+  return;
+
+  <div>Accordion</div>;
+}
